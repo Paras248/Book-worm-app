4dba56b90c0c5ce6a9aa578e30556eaf65baa156
Paras248 <60874409+Paras248@users.noreply.github.com>
2022-04-26 13:23:05 +0530

    Updated app.js

--- App.js
+++ App.js
@@ -3,12 +3,12 @@ import Providers from "./src/navigation/Providers";
 import firebase from "firebase";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyAGJOtNo6bNdGvEM5XficJXSqtWb5q2xK8",
+    apiKey: process.env.APIKEY,
     authDomain: "book-app-authentication.firebaseapp.com",
     projectId: "book-app-authentication",
     storageBucket: "book-app-authentication.appspot.com",
     messagingSenderId: "379041237722",
-    appId: "1:379041237722:web:39237cf0ea3338e80173e3"
+    appId: process.env.APPID
 };
 
 if (!firebase.apps.length) {
